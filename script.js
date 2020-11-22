412d68b5278e38a9a8c3db795d13c1458958c9a7
Amanda Anderson <aanderson_120@yahoo.com>
2020-11-21 19:55:22 -0600

    append div

--- script.js
+++ script.js
@@ -1,26 +1,46 @@
 $(function () {
 
-    // $(".yesBtn").on("click", function () {
-    //     let item = $(this).closest('p.item');
-    //     console.log(item)
-    //     item.appendTo("#yesList");
-    //     item.append("<button id='delete'>Delete</button>")
-    // }); 
-
     $(".yesBtn").on("click", function () {
-        let item = $(this).closest('p.item');
+        let item = $(this).closest('div');
         console.log(item)
-        item.appendTo("#yesList");
-        item.append("<button id='delete'>Delete</button>")
-    }); 
+        $("#yesList").append(item);
+        // if ($('#delete').length > 0) {
+        //     $("#yesList").append(item);
+        // } else {
+        //     $("#yesList").append(item);
+        //     item.append("<button id='delete' class='btn btn-secondary btn-sm'>Delete</button>")
+        // }
+    });
+
+    $(".maybeBtn").on("click", function () {
+        let item = $(this).closest('div');
+        console.log(item)
+        $("#maybeList").append(item);
+        // if ($('#delete').length > 0) {
+        //     $("#maybeList").append(item);
+        // } else {
+        //     $("#maybeList").append(item);
+        //     item.append("<button id='delete' class='btn btn-secondary btn-sm'>Delete</button>")
+        // }
+    });
+
+    $(".noBtn").on("click", function () {
+        let item = $(this).closest('div');
+        console.log(item)
+        $("#noList").append(item);
+        // if ($('#delete').length > 0) {
+        //     $("#noList").append(item);
+        // } else {
+        //     $("#noList").append(item);
+        //     item.append("<button id='delete' class='btn btn-secondary btn-sm'>Delete</button>")
+        // }
+    });
+
+    // delete function
+    // $(document).on('click', '#delete', function () {
+    //     console.log('gonna do it')
+    //     $(this).closest('div').remove();
+    // });
 
 
-
-
-
-
-        // delete function
-        $('#yesList').on('click', '.delete', function(){
-            $(".noBtn").closest('p').remove();
-        });
 })
